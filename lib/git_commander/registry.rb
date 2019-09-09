47e7ce1b66af3a6666491fb19e2dac2ee868e8d5
Valentino Stoll <v@codenamev.com>
2019-09-09 08:57:15 -0400

    Allows naming registries

--- lib/git_commander/registry.rb
+++ lib/git_commander/registry.rb
@@ -5,7 +5,7 @@ module GitCommander
   class Registry
     class CommandNotFound < StandardError; end
 
-    attr_accessor :commands
+    attr_accessor :commands, :name
 
     def initialize
       @commands = {}
@@ -13,18 +13,18 @@ module GitCommander
 
     # Adds a command to the registry
     #
-    # @param [String, Symbol] name the name of the command to add to the
+    # @param [String, Symbol] command_name the name of the command to add to the
     #   registry
-    def register(name, **options, &block)
-      command_name = name.to_sym
+    def register(command_name, **options, &block)
+      command_name = command_name.to_sym
 
-      GitCommander.logger.debug "[registry] Registering command `#{command_name}` with args: #{options.inspect}..."
-      commands[command_name] = GitCommander::Command.new(command_name, **options.merge(block: block))
+      GitCommander.logger.debug "[#{logger_tag}] Registering command `#{command_name}` with args: #{options.inspect}..."
+      commands[command_name] = GitCommander::Command.new(command_name, registry: self, **options.merge(block: block))
     end
 
     # Looks up a command in the registry
     #
-    # @param [String, Symbol] name the name of the command to look up in the
+    # @param [String, Symbol] command_name the name of the command to look up in the
     #   registry
     #
     # @example Fetch a command from the registry
@@ -34,12 +34,18 @@ module GitCommander
     #
     # @raise [CommandNotFound] when no command is found in the registry
     # @return [GitCommander::Command, #run] a command object that responds to #run
-    def find(name)
-      GitCommander.logger.debug "[registry] looking up command: #{name.inspect}"
-      command = commands[name.to_s.to_sym]
-      raise CommandNotFound, "#{name} does not exist in the registry" if command.nil?
+    def find(command_name)
+      GitCommander.logger.debug "[#{logger_tag}] looking up command: #{command_name.inspect}"
+      command = commands[command_name.to_s.to_sym]
+      raise CommandNotFound, "[#{logger_tag}] #{command_name} does not exist in the registry" if command.nil?
 
       command
+    end
+
+    private
+
+    def logger_tag
+      [name, "registry"].compact.join(" ")
     end
   end
 end
