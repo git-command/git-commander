1f33ec447d3f4dd9862c777f2b6e3116fd3156b6
Valentino Stoll <v@codenamev.com>
2019-09-11 08:49:45 -0400

    Refactors Command to parse arguments/flags/switches as array of objects and adds help command to allow for command self-documentation

--- lib/git_commander/command.rb
+++ lib/git_commander/command.rb
@@ -3,18 +3,36 @@
 module GitCommander
   # @abstract Wraps domain logic for executing git-cmd commands
   class Command
-    attr_reader :arguments, :block, :description, :flags, :switches, :name
+    attr_reader :arguments, :block, :description, :flags, :name, :summary, :switches
     attr_accessor :output
+
+    # @nodoc
+    class Option
+      attr_reader :default, :description, :name
+      def initialize(name:, default: nil, description: nil)
+        @name = name.to_sym
+        @default = default
+        @description = description
+      end
+
+      def ==(other)
+        other.class == self.class &&
+          other.name == name &&
+          other.default == default &&
+          other.description == description
+      end
+      alias eql? ==
+    end
 
     def initialize(name, registry: nil, **options)
       @name = name
-      @arguments = parse_array_or_hash(options[:arguments] || {})
-      @flags = parse_array_or_hash(options[:flags] || {})
-      @switches = parse_array_or_hash(options[:switches] || {})
-      @description = options[:description] || ""
+      @description = options[:description]
+      @summary = options[:summary]
       @block = options[:block] || proc {}
       @registry = registry || GitCommander::Registry.new
       @output = options[:output] || STDOUT
+
+      parse_options(options)
     end
 
     def run(args = [])
@@ -25,12 +43,61 @@ module GitCommander
       output.puts message
     end
 
+    def help
+      say "NAME"
+      say "    git-cmd #{name} – #{summary}"
+      say "USAGE"
+      say "    git-cmd #{name} [command options] #{arguments.map { |arg| "[#{arg.name}]" }.join(" ")}"
+      description_help
+      argument_help
+      options_help
+    end
+
     private
 
-    def parse_array_or_hash(array_or_hash)
-      return array_or_hash if array_or_hash.is_a?(Hash)
+    def parse_options(options)
+      @arguments = Array(options[:arguments]).map { |a| Option.new(**a) }
+      @flags = Array(options[:flags]).map { |f| Option.new(**f) }
+      @switches = Array(options[:switches]).map { |s| Option.new(**s) }
+    end
 
-      array_or_hash.each_with_object({}) { |key, hash| hash[key] = nil }
+    def description_help
+      return if description.to_s.empty?
+
+      say "DESCRIPTION"
+      say "    #{description}"
+    end
+
+    def argument_help
+      return unless arguments.any?
+
+      say "ARGUMENTS"
+      arguments.each do |argument|
+        default_text = argument.default.nil? ? "" : "(default: #{argument.default.inspect}) "
+        say "    #{argument.name} – #{default_text}#{argument.description}"
+      end
+    end
+
+    def options_help
+      return unless flags.any? || switches.any?
+
+      say "COMMAND OPTIONS"
+      flag_help
+      switch_help
+    end
+
+    def flag_help
+      flags.each do |flag|
+        flag_names = ["-#{flag.name.to_s[0]}", "--#{flag.name}"]
+        say "    #{flag_names} – #{flag.default.nil? ? "" : "(default: #{flag.default})  "}#{flag.description}"
+      end
+    end
+
+    def switch_help
+      switches.each do |switch|
+        switch_names = [switch.name.to_s[0], "-#{switch.name}"].map { |s| "-#{s}" }.join(", ")
+        say "    #{switch_names} – #{switch.default.nil? ? "" : "(default: #{switch.default})  "}#{switch.description}"
+      end
     end
   end
 end
--- spec/lib/git_commander/command_spec.rb
+++ spec/lib/git_commander/command_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "spec_helper"
+
 describe GitCommander::Command do
   it "runs the block registered to it"
   it "runs the block registered to it passing arguments"
@@ -11,5 +13,63 @@ describe GitCommander::Command do
     command = described_class.new(:wtf, output: output)
     command.say "Ooh eeh what's up with that"
     expect(output).to have_received(:puts).with "Ooh eeh what's up with that"
+  end
+
+  it "can output a help message" do
+    output = spy("output")
+    full_command = described_class.new(
+      :start,
+      arguments: [
+        {
+          name: :feature_branch,
+          default: "new-feature",
+          description: "name of the new feature branch"
+        },
+        {
+          name: :tracking_id,
+          description: "id of the story to track"
+        }
+      ],
+      flags: [
+        {
+          name: :base,
+          description: "name of a branch you want to branch off of"
+        }
+      ],
+      switches: [
+        {
+          name: :silent,
+          default: false,
+          description: "supress output"
+        }
+      ],
+      output: output,
+      summary: "This will create a new feature branch and setup remote tracking",
+      description: <<~LONGTIME
+        Performs the following:\n
+        \t$ git checkout <base_branch>\n
+        \t$ git pull origin <base_branch>\n
+        \t$ git push origin <base_branch>:refs/heads/[new_feature_branch]\n
+        \t$ git checkout --track -b [new_feature_branch] origin/[new_feature_branch]\n
+      LONGTIME
+    )
+
+    full_command.help
+
+    expect(output).to have_received(:puts).with("NAME").ordered
+    expect(output).to have_received(:puts).with("    git-cmd start – #{full_command.summary}").ordered
+    expect(output).to have_received(:puts).with("USAGE").ordered
+    expect(output).to have_received(:puts).with(
+      "    git-cmd start [command options] [feature_branch] [tracking_id]"
+    ).ordered
+    expect(output).to have_received(:puts).with("DESCRIPTION").ordered
+    expect(output).to have_received(:puts).with("    #{full_command.description}").ordered
+    expect(output).to have_received(:puts).with("ARGUMENTS").ordered
+    expect(output).to have_received(:puts).with(
+      "    feature_branch – (default: \"new-feature\") name of the new feature branch"
+    ).ordered
+    expect(output).to have_received(:puts).with("    tracking_id – id of the story to track").ordered
+    expect(output).to have_received(:puts).with("COMMAND OPTIONS")
+    expect(output).to have_received(:puts).with("    [\"-b\", \"--base\"] – name of a branch you want to branch off of")
   end
 end
