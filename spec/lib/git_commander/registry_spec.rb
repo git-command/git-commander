34237813113f321e16423face643fd2168f269c4
Valentino Stoll <vstoll@doximity.com>
2019-09-11 10:29:45 -0400

    Refactors Command to parse arguments/flags/switches as array of objects and adds help command to allow for command self-documentation

--- spec/lib/git_commander/registry_spec.rb
+++ spec/lib/git_commander/registry_spec.rb
@@ -13,42 +13,73 @@ describe GitCommander::Registry do
   end
 
   it "allows registering commands with arguments" do
-    registry.register :wtf, arguments: { first: nil, second: nil }
+    registry.register :wtf, arguments: [{ name: :first }, { name: :second }]
     wtf_command = registry.commands[:wtf]
     expect(wtf_command).to be
-    expect(wtf_command.arguments).to eql(first: nil, second: nil)
+    expect(wtf_command.arguments).to eql(
+      [
+        GitCommander::Command::Option.new(name: :first, default: nil, description: nil),
+        GitCommander::Command::Option.new(name: :second, default: nil, description: nil)
+      ]
+    )
   end
 
   it "allows registering commands with flags" do
-    registry.register :wtf, arguments: %i[first second], flags: { raise_hell: false }
+    registry.register :wtf,
+                      arguments: [{ name: :first }, { name: :second }],
+                      flags: [{ name: "raise_hell", default: false }]
     wtf_command = registry.commands[:wtf]
     expect(wtf_command).to be
-    expect(wtf_command.arguments).to eql(first: nil, second: nil)
-    expect(wtf_command.flags).to eql(raise_hell: false)
+    expect(wtf_command.arguments).to eql(
+      [
+        GitCommander::Command::Option.new(name: :first, default: nil, description: nil),
+        GitCommander::Command::Option.new(name: :second, default: nil, description: nil)
+      ]
+    )
+    expect(wtf_command.flags).to eql(
+      [GitCommander::Command::Option.new(name: :raise_hell, default: false)]
+    )
   end
 
   it "allows registering commands with switches" do
-    registry.register :wtf, arguments: %i[first second], flags: { raise_hell: false }, switches: { clobber: true }
+    registry.register :wtf,
+                      arguments: [{ name: :first }, { name: :second }],
+                      flags: [{ name: "raise_hell", default: false }],
+                      switches: [{ name: "clobber", default: true }]
     wtf_command = registry.commands[:wtf]
     expect(wtf_command).to be
-    expect(wtf_command.arguments).to eql(first: nil, second: nil)
-    expect(wtf_command.flags).to eql(raise_hell: false)
-    expect(wtf_command.switches).to eql(clobber: true)
+    expect(wtf_command.arguments).to eql(
+      [
+        GitCommander::Command::Option.new(name: :first, default: nil, description: nil),
+        GitCommander::Command::Option.new(name: :second, default: nil, description: nil)
+      ]
+    )
+    expect(wtf_command.flags).to eql(
+      [GitCommander::Command::Option.new(name: :raise_hell, default: false)]
+    )
+    expect(wtf_command.switches).to eql(
+      [GitCommander::Command::Option.new(name: :clobber, default: true)]
+    )
   end
 
   it "allows describing a command" do
-    registry.register(:wtf, description: "WTF is up with x")
+    registry.register(:wtf, summary: "WTF yo", description: "WTF is up with x")
     wtf_command = registry.commands[:wtf]
     expect(wtf_command).to be
+    expect(wtf_command.summary).to eq("WTF yo")
     expect(wtf_command.description).to eq("WTF is up with x")
   end
 
   it "allows re-registering existing commands" do
-    registry.register(:wtf, arguments: { day: :today })
+    registry.register(:wtf, arguments: [{ name: :day, default: :today }])
     wtf_command = registry.commands[:wtf]
-    expect(wtf_command.arguments).to eq(day: :today)
-    registry.register(:wtf, arguments: { message: nil })
-    expect(registry.find(:wtf).arguments).to eq(message: nil)
+    expect(wtf_command.arguments).to eql(
+      [GitCommander::Command::Option.new(name: :day, default: :today, description: nil)]
+    )
+    registry.register(:wtf, arguments: [{ name: :message }])
+    expect(registry.find(:wtf).arguments).to eq(
+      [GitCommander::Command::Option.new(name: :message, default: nil, description: nil)]
+    )
   end
 
   it "allows looking up a registered command" do
