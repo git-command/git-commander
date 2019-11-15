8a387217d81e0bcef180ce562bd5f8db91b0cc63
Valentino Stoll <v@codenamev.com>
2019-11-15 08:36:25 -0500

    Add initial feature spec with test of help command

--- spec/features/run_command_with_arguments_spec.rb
+++ spec/features/run_command_with_arguments_spec.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "../support/command_helpers"
 
 RSpec.describe "Run command with arguments" do
-  before do
+  before :all do
+    setup_environment
+  end
+
+  after :all do
+    command_helpers_teardown
+  end
+
+  describe "help command" do
+    it "outputs the name, description, version number and usage for the git-cmd command" do
+      run_system_call "#{git_cmd_path} help"
+      expect(last_command.output).
+        to include("git-cmd – Git Commander allows running custom git commands from a centralized location")
+      expect(last_command.output).
+        to include(GitCommander::VERSION)
+      expect(last_command.output).
+        to include("git-cmd command [command options] [arguments...]")
+    end
   end
 end
