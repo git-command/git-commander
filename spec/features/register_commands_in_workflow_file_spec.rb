535dbdfce12b1fcf1b804ea98e94e5cffaaa2746
Valentino Stoll <v@codenamev.com>
2020-02-05 08:59:59 -0500

    Update git-cmd to check for commands to load from Workflow file and .git-commands directory

---
+++ spec/features/register_commands_in_workflow_file_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Run command with arguments" do
+  before :all do
+    setup_environment
+  end
+
+  after :all do
+    command_helpers_teardown
+  end
+
+  describe "command help" do
+    it "displays help text for commands registered in Workflow file" do
+      FileUtils.cp "#{fixtures_dir}/workflow_example.rb", "#{project_dir}/Workflow"
+
+      run_system_call "#{git_cmd_path} help"
+      expect(last_command.output).
+        to include("hello, faq")
+
+      run_system_call "#{git_cmd_path} faq --help"
+      expect(last_command.output).
+        to include("git-cmd faq [command options] [question] [answer]")
+      expect(last_command.output).to include("-w WITH_QUESTION")
+      expect(last_command.output).to include("--with-question")
+      expect(last_command.output).to include("-l, --[no-]loud")
+    end
+
+    it "displays help text for commands registered in .git-commands directory" do
+      FileUtils.mkdir "#{project_dir}/.git-commands"
+      FileUtils.cp "#{fixtures_dir}/faq_command.rb", "#{project_dir}/.git-commands/faq.rb"
+
+      run_system_call "#{git_cmd_path} help"
+      expect(last_command.output).
+        to include("hello, faq")
+
+      run_system_call "#{git_cmd_path} faq --help"
+      expect(last_command.output).
+        to include("git-cmd faq [command options] [question] [answer]")
+      expect(last_command.output).to include("-w WITH_QUESTION")
+      expect(last_command.output).to include("--with-question")
+      expect(last_command.output).to include("-l, --[no-]loud")
+    end
+
+    it "ignores loading commands from .git-commands directory if it doesn't exist" do
+      FileUtils.rm_rf "#{project_dir}/Workflow"
+      FileUtils.rm_rf "#{project_dir}/.git-commands"
+      run_system_call "#{git_cmd_path} help"
+
+      expect(last_command.output).to_not include("hello, faq")
+    end
+  end
+end
