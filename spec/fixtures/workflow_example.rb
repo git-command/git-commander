e626ce6ad32295b4c585513ca71a8a27cb50d06e
Valentino Stoll <v@codenamev.com>
2020-02-05 08:06:32 -0500

    Adds missing workflow example fixture

---
+++ spec/fixtures/workflow_example.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+command :hello do |cmd|
+  cmd.on_run do |_options|
+    say "Hello."
+  end
+end
+
+command :faq do |cmd|
+  cmd.summary "Outputs a question and answer combination."
+  cmd.description "This is way too much information about a simple FAQ section."
+  cmd.argument :question
+  cmd.argument :answer, default: "Because racecar."
+  cmd.flag :with_question, default: false
+  cmd.switch :loud, default: false
+
+  cmd.on_run do |options|
+    say "Question: #{options[:question]}?" if options[:with_question]
+    response = options[:loud] ? options[:answer].upcase : options[:answer]
+    say "Answer: #{response}"
+  end
+end
