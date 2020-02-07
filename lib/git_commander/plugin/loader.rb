bd2dac703547d49cfad7b62fafc3b9c9dbc0725b
Valentino Stoll <vstoll@doximity.com>
2020-02-07 08:28:26 -0500

    Adds new Plugin loader to load native plugin files

---
+++ lib/git_commander/plugin/loader.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "bundler/inline"
+require_relative "../loader"
+
+module GitCommander
+  class Plugin
+    # @abstract Handles loading native plugins by name.
+    class Loader < ::GitCommander::Loader
+      class NotFoundError < StandardError; end
+      class LoadError < StandardError; end
+
+      NATIVE_PLUGIN_DIR = File.expand_path(File.join(__dir__, "..", "plugins"))
+
+      attr_reader :content
+
+      def load(name)
+        @content = File.read("#{NATIVE_PLUGIN_DIR}/#{name}.rb")
+        result.plugins << GitCommander::Plugin.new(name.to_sym, source_instance: instance_eval(@content))
+        result
+      rescue Errno::ENOENT => e
+        handle_error LoadError, e
+      rescue StandardError => e
+        handle_error NotFoundError, e
+      end
+
+      private
+
+      def handle_error(error_klass, original_error)
+        error = error_klass.new(original_error.message)
+        error.set_backtrace original_error.backtrace
+        @result.errors << error
+        @result
+      end
+    end
+  end
+end
