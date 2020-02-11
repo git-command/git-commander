f6bc67010dca202f62759eb64658322de5aa2241
Valentino Stoll <vstoll@doximity.com>
2020-02-10 22:33:07 -0700

    Allows plugins to set their own commands

--- lib/git_commander/plugin/loader.rb
+++ lib/git_commander/plugin/loader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "bundler/inline"
+require_relative "../command/configurator"
 require_relative "../loader"
 
 module GitCommander
@@ -12,16 +13,29 @@ module GitCommander
 
       NATIVE_PLUGIN_DIR = File.expand_path(File.join(__dir__, "..", "plugins"))
 
-      attr_reader :content
+      attr_reader :content, :plugin, :commands
+
+      def initialize(registry)
+        @commands = []
+        super
+      end
 
       def load(name)
         @content = File.read("#{NATIVE_PLUGIN_DIR}/#{name}.rb")
-        result.plugins << GitCommander::Plugin.new(name.to_sym, source_instance: instance_eval(@content))
+        @plugin = GitCommander::Plugin.new(name.to_sym, source_instance: instance_eval(@content))
+        @plugin.commands = @commands
+        result.plugins << @plugin
         result
       rescue Errno::ENOENT, Errno::EACCES => e
         handle_error LoadError, e
       rescue StandardError => e
         handle_error NotFoundError, e
+      end
+
+      def command(name, &block)
+        @commands << Command::Configurator.new(registry).configure(name, &block)
+      rescue Command::Configurator::ConfigurationError => e
+        result.errors << e
       end
 
       private
--- spec/lib/git_commander/plugin/loader_spec.rb
+++ spec/lib/git_commander/plugin/loader_spec.rb
@@ -21,6 +21,32 @@ RSpec.describe GitCommander::Plugin::Loader do
       expect(plugin.executor).to respond_to(:run)
     end
 
+    it "loads commands for the plugin" do
+      allow(File).to receive(:read).and_return(
+        <<~COMMAND
+          require 'ostruct'
+
+          command :hello do |cmd|
+            cmd.on_run { say "hello" }
+          end
+
+          command :blank do |cmd|
+          end
+
+          OpenStruct.new(run: true)
+        COMMAND
+      )
+
+      result = loader.load(:plugin_with_commands)
+
+      expect(result).to be_success
+
+      plugin = result.plugins.first
+      expect(plugin.name).to eq :plugin_with_commands
+      expect(plugin.commands.size).to eq 2
+      expect(plugin.executor).to respond_to(:run)
+    end
+
     it "reports NotFound if the native plugin doesn't exist" do
       result = loader.load(:bubbles)
 
@@ -41,6 +67,25 @@ RSpec.describe GitCommander::Plugin::Loader do
       resulting_error = result.errors.first
       expect(resulting_error).to be_kind_of described_class::LoadError
       expect(resulting_error.message).to include "Permission denied"
+      expect(resulting_error.backtrace).to_not be_empty
+    end
+
+    it "reports Configurator::ConfigurationError if a plugin command cannot be parsed" do
+      allow(File).to receive(:read).and_return(
+        <<~COMMAND
+          command :danger do |cmd|
+            cmd.danger!
+          end
+        COMMAND
+      )
+
+      result = loader.load(:danger)
+
+      expect(result).to_not be_success
+
+      resulting_error = result.errors.first
+      expect(resulting_error).to be_kind_of GitCommander::Command::Configurator::ConfigurationError
+      expect(resulting_error.message).to include "undefined method"
       expect(resulting_error.backtrace).to_not be_empty
     end
   end
