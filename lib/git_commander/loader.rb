74559993f17a0078f5aebcf728fd8b0b3094d96f
Valentino Stoll <vstoll@doximity.com>
2020-03-18 08:52:09 -0400

    Adds GitCommander::Loader#system

--- lib/git_commander/loader.rb
+++ lib/git_commander/loader.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require "bundler/inline"
 require_relative "loader_result"
 
 module GitCommander
   # @abstract The interface class outlining requirements for an operational Loader
   class Loader
+    # Let the loaders proxy system calls in the git-commander context
+    undef :system
+
     attr_reader :registry, :result
 
     def initialize(registry)
@@ -15,6 +19,10 @@ module GitCommander
     # Expected to return an instance of GitCommander::LoaderResult
     def load(_options = {})
       raise NotImplementedError
+    end
+
+    def system
+      GitCommander::System
     end
   end
 end
--- spec/lib/git_commander/loader_spec.rb
+++ spec/lib/git_commander/loader_spec.rb
@@ -4,8 +4,22 @@ require "spec_helper"
 
 RSpec.describe GitCommander::Loader do
   describe "#load" do
+    let(:loader) { described_class.new GitCommander::Registry.new }
+
     it "raises a NotImplementedError" do
-      expect { described_class.new(GitCommander::Registry.new).load }.to raise_error NotImplementedError
+      expect { loader.load }.to raise_error NotImplementedError
+    end
+
+    it "provides access to running system commands" do
+      expected_command_string = 'echo "Good vibrations"'
+      expect(GitCommander::System).to receive(:run).with(expected_command_string)
+      loader.system.run expected_command_string
+    end
+
+    it "allows output to the system" do
+      expected_output = "Good vibrations"
+      expect(GitCommander::System).to receive(:say).with(expected_output)
+      loader.system.say expected_output
     end
   end
 end
